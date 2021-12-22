7516d450033d6c12c6ef587e147c4cd03a333b79
Nathan <nwolf2@oswego.edu>
2021-12-22 09:52:42 -0500

    initial commit

---
+++ Onondaga.js
@@ -0,0 +1,60 @@
+function pullMiddleDescription() {  // contains only everything between 13 and 53 string positions 
+  var app = SpreadsheetApp;  // Gets Google Sheets App
+  var thisSpreadSheet = app.getActiveSpreadsheet();  // Gets Active Spreadssheet 
+
+  var main = thisSpreadSheet.getSheets()[1];  // gets an array with the index referencencing the respective sheets
+
+  rowTotal = main.getLastRow();
+  console.log(rowTotal);
+
+  for(let i = 2; i < rowTotal; i++){
+        let cellPreParseValue = main.getRange(i, 1).getValue();
+        let parsedDescription = cellPreParseValue.substring(13,53).trim();
+        Logger.log(parsedDescription);
+        main.getRange(i, 5).setValue(parsedDescription);
+
+  }
+
+  return 0;
+}
+
+function pullLastPart() {  // Omits everything before 54 position in cell string
+  var app = SpreadsheetApp;  // Gets Google Sheets App
+  var thisSpreadSheet = app.getActiveSpreadsheet();  // Gets Active Spreadssheet 
+
+  var main = thisSpreadSheet.getSheets()[1];  // gets an array with the index referencencing the respective sheets
+
+  rowTotal = main.getLastRow();
+  console.log(rowTotal);
+
+  for(let i = 2; i < rowTotal; i++){
+        let cellPreParseValue = main.getRange(i, 1).getValue();
+        let parsedDescription = cellPreParseValue.substring(54).trim();
+        Logger.log(parsedDescription);
+        main.getRange(i, 6).setValue(parsedDescription);
+
+  }
+
+  return 0;
+}
+
+
+function convertFACPRegexToColumn() {  // simplies copies value from one to call another (uses to copy cell values that used a formula inside excel)
+  var app = SpreadsheetApp;  // Gets Google Sheets App
+  var thisSpreadSheet = app.getActiveSpreadsheet();  // Gets Active Spreadssheet 
+
+  var main = thisSpreadSheet.getSheets()[1];  // gets an array with the index referencencing the respective sheets
+
+  rowTotal = main.getLastRow();
+  console.log(rowTotal);
+
+  for(let i = 2; i < rowTotal; i++){
+        let cellPreParseValue = main.getRange(i, 4).getValue();
+        // let parsedDescription = cellPreParseValue.substring(54).trim();
+        Logger.log(cellPreParseValue);
+        main.getRange(i, 3).setValue(cellPreParseValue);
+
+  }
+
+  return 0;
+}
---
+++ SimplexConsolidatingV1v1.js
@@ -0,0 +1,95 @@
+function mergeAndParse() {
+
+  const _DEVICEKeys = new Map([  // Map to check for device keys and assign them description
+["ADRPUL:PULL", "Pull Station"], ["COMBO:UTILITY", "Detector Photo Smoke/Heat/Gas"],
+["GENIAM:SO", "Shut Off Valve"], ["GENIAM:WATER", "Flow Switch"],
+["HEAT DETECTOR:HEAT DETECTOR", "Detector Heat"], ["HEAT DETECTOR:HEAT DETECTOR DETECTOR", "Detector Heat"],
+["IAM:SO", "Shut Off Valve"], ["IAM:WATER", "Flow Switch"],
+["IAM:HEAT DETECTOR", "Detector Heat"], ["IAM:WSO", "Shut Off Valve/Flow Switch"],
+["ICOMBO:UTILITY", "Detector Photo Smoke/Gas w/Isolator"], ["IHEAT:DETECTOR HEAT DETECTOR", "Detector Heat"],
+["MCOH:UTILITY", "Detector Heat/Gas"],["MCOP:UTILITY", "Detector Photo Smoke/Gas"], 
+["MCOPS:UTILITY", "Detector Photo Smoke/Gas w/Signal"], ["PHOTO:SMOKE DETECTOR", "Detector Photo Smoke"], 
+["PHOTO:DUCT", "Detector Duct Photo Smoke"], ["RPHOTO:DUCT", "Detector Duct Photo Smoke"]
+  ]);
+
+  let app = SpreadsheetApp;  // Gets Google Sheets Applicaion 
+  let currentSpreadsheetFile = app.getActiveSpreadsheet();  // Gets Active Spreadssheet - not technicall neccesary
+  // if running script within spreadsheet otherwise would reference spreadsheet uri directly 
+
+  let numberOfSheets = currentSpreadsheetFile.getNumSheets();  // gets total of numbers of sheets in spreadsheets
+  let sheets = currentSpreadsheetFile.getSheets();  // gets an array with the index referencencing the respective sheets
+  let assetListSheet = currentSpreadsheetFile.getSheets()[0]; // sets the first of assetlist to assetList sheet letiable 
+  let assetRows = assetListSheet.getLastRow();
+  console.log(assetRows);
+  
+  // assetListSheet.clear();  // using mostly for testing - removed when doing progressively runnig scripts 
+
+  let sheetNames = [];  // stores names of sheets, more for debugging 
+
+  for (let i = 0 ; i < numberOfSheets ; i++){  // pushes sheet names
+       sheetNames.push(sheets[i].getName());  
+  }
+ 
+  Logger.log("Sheet Names: " + sheetNames);  // outputs arrays of sheet name 
+
+  Logger.log("Before sheet loop");
+
+  for (let i = 2; i < numberOfSheets ; i ++){  // starts at one otherwise starts at 'master list sheet which
+    // is usuaully ones, otherwise first sheet is '0' as expected 
+
+    // Activates Data Related Sheets
+    let tempActiveSheet = currentSpreadsheetFile.getSheetByName(sheetNames[i]);  // stores sheet from position in loop
+    tempActiveSheet.activate();  // not techicall neccesary but activate the current sheet in question (brings it into view on the user's screen)
+    Logger.log(tempActiveSheet.getName());  // simply gets the currently worked on sheets names 
+
+    rowsInSheet = tempActiveSheet.getLastRow();
+    Logger.log("Current # Values in AssetList: " + rowsInSheet);  // Outputs total rows in sheet that contain data, bear in mind will
+      // look for data until are columns and rows are empty 
+
+
+      // Status Area
+      Logger.log("Processing Sheet #" + i);
+      Logger.log("Sheet name" + tempActiveSheet);
+    
+    for (let row = rowsInSheet ; row > 0 ; row --)
+       {   
+        Logger.log(row + "/" + rowsInSheet);
+
+      let t_Address = getValasStringTrim(tempActiveSheet, row, 1);
+      let t_Device = getValasStringTrim(tempActiveSheet, row, 2);
+      let t_Device2 = getValasStringTrim(tempActiveSheet, row, 3);
+      let t_Description = getValasStringTrim(tempActiveSheet, row, 4);
+      let t_Location = getValasStringTrim(tempActiveSheet, row, 5);
+      let t_Property = getValasStringTrim(tempActiveSheet, row, 6);
+
+      let t_concatDevice = t_Device + ":" + t_Device2;  // merges column 2 and 3 together to form key value for MCOR map
+    
+      // Holds FACP with Description value 
+      let t_Address_wDescription = "";
+
+      if(_DEVICEKeys.has(t_concatDevice)){
+      t_Address_wDescription = t_Address + " " + _DEVICEKeys.get(t_concatDevice);
+
+      }
+      else{
+      t_Address_wDescription = null; // sets to null to skip lines without a descrption (may contain superflous entry data in other columns)
+      }
+ 
+    let addedRange = [getRndInteger(1000000000, 9999999999), t_Address_wDescription, t_Description, 28230, "MAIN CAMPUS", t_Property, t_Location, "LIFE SAFETY", "FIRE DETECTION-ALARM"];
+    
+    if(t_Address_wDescription != null)
+      {
+      assetListSheet.appendRow(addedRange); 
+      }
+      tempActiveSheet.deleteRow(row);
+      }
+  }
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function getValasStringTrim(Sheet, row, col){
+  return Sheet.getRange(row, col).getValue().toString().trim();
+}
