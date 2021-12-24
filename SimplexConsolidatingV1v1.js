4ea7b21e239ff4988519545c8b3dd131d70ecdae
Nathan <nwolf2@oswego.edu>
2021-12-24 18:14:53 -0500

    moved helper functions out of Simplex to Helpers.js

--- SimplexConsolidatingV1v1.js
+++ SimplexConsolidatingV1v1.js
@@ -86,11 +86,3 @@ function mergeAndParse() {
     }
   }
 }
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-function getValasStringTrim(Sheet, row, col){
-  return Sheet.getRange(row, col).getValue().toString().trim();
-}
