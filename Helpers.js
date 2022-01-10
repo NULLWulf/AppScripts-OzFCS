869c6f748b31ea70f09806f897ec7c2325b5cb7f
Nathan <nwolf2@oswego.edu>
2022-01-10 15:38:57 -0500

    started reversal script to pull out extra data

--- Helpers.js
+++ Helpers.js
@@ -5,3 +5,26 @@ function getRndInteger(min, max) {
   function getValasStringTrim(Sheet, row, col){
     return Sheet.getRange(row, col).getValue().toString().trim();
   }
+
+  function getMapKeyFromRegexLeft(){
+
+    var regex1 = new RegExp('M1-\d{1,3}-1');
+    var regex2 = new RegExp('M1-\d{1,3}-2');
+    var regex3 = new RegExp('M1-\d{1,3}-3');
+    var regex4 = new RegExp('M1-\d{1,3}-4');
+    var regex5 = new RegExp('M1-\d{1,3}-5');
+
+    
+  }
+
+  
+  function getMapKeyFromRegexRight(){
+
+    var regex1 = new RegExp('M1-\d{1,3}-1');
+    var regex2 = new RegExp('M1-\d{1,3}-2');
+    var regex3 = new RegExp('M1-\d{1,3}-3');
+    var regex4 = new RegExp('M1-\d{1,3}-4');
+    var regex5 = new RegExp('M1-\d{1,3}-5');
+
+
+  }
